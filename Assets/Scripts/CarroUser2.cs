57df5cd4f1d289f909f89206e22bfd98c6910ad8
Fabián Balmaceda Rescia <fabianbalmaceda2@gmail.com>
2017-12-09 16:46:17 -0600

    carro2
    carro2

---
+++ Assets/Scripts/CarroUser2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class CarroUser2 : MonoBehaviour
+{
+	public float speed;
+	public float constSpeed;
+	public float HightSpeed;
+
+	public float tilt;
+
+	private float velocity;
+
+	private Rigidbody rigidbody;
+
+	void Awake ()
+	{
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+
+		if (moveVertical > 0) {
+			moveVertical += HightSpeed;
+
+		} else {
+			moveVertical += constSpeed;
+		}
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement * speed;
+
+		rigidbody.rotation = Quaternion.Euler (0.0f, rigidbody.velocity.x * -tilt, 0.0f);
+	}
+}
