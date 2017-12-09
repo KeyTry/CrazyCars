7c502eec0bb1d77a130293b7299e567a655e5808
Daniel <sq.daniel@gmail.com>
2017-12-09 09:53:07 -0600

    Acera Script

---
+++ Assets/Scripts/Acera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Acera : MonoBehaviour {
+
+	public GameObject[] Edificios;
+
+	Transform thisTransform;
+
+	Vector3 arriba;
+	Vector3 centro;
+	Vector3 abajo;
+
+	// Use this for initialization
+	void Start () {
+		thisTransform = gameObject.transform;
+		Instantiate (Edificios[1],gameObject.transform);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
