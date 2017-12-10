8ced09c4c5e8dbe776be08f8f8cd74e3f437d11b
Daniel Somarribas <sq.daniel@gmail.com>
2017-12-09 18:09:58 -0600

    GameManager

---
+++ Assets/Scripts/WrongWay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WrongWay : MonoBehaviour {
+
+	public Text points;
+	public GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		gameManager = GameObject.Find ("GameManager").GetComponent<GameManager>();
+		points = GameObject.Find ("Text").GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerStay(Collider other)
+	{
+		Debug.Log ("Something in collision");
+		if (other.CompareTag ("Player")) {
+			Debug.Log ("Wrong Way!");
+			gameManager.points++;
+			Debug.Log (gameManager.points);
+			points.text = "Puntos: " + gameManager.points;
+		}
+	}
+}
