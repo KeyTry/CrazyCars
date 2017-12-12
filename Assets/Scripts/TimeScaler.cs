47199eab738cba10bd3053268292c232c3bbb5ac
Daniel Somarribas <sq.daniel@gmail.com>
2017-12-11 21:51:17 -0600

    Some slowmo improvements

--- Assets/Scripts/TimeScaler.cs
+++ Assets/Scripts/TimeScaler.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class TimeScaler : MonoBehaviour {
 	private float input;
+    public Rigidbody car;
 	void Start(){
 		Time.timeScale = 0.4f;
 	}
-	void Update () {
+	void FixedUpdate () {
 		input = Input.GetAxis ("Vertical");
 
-		if (input > 0) {
-			Time.timeScale = 0.6f;
-        } 
-        if (input == 0) {
-			Time.timeScale = 0.4f;
+        if (input > 0)
+        {
+            Time.timeScale = 0.6f;
+        }
+        if (input == 0)
+        {
+            Time.timeScale = 0.5f;
         }
         if (input < 0)
         {
             Time.timeScale = 0.2f;
+            if (car.velocity.magnitude == 0)
+            {
+                Time.timeScale = 0.05f;
+            }
         }
+
         Time.fixedDeltaTime = 0.02F * Time.timeScale;
     }
 }
