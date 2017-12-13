f4f54cf495ca83c8abf0331897e5e539ca952eb6
Daniel Somarribas <sq.daniel@gmail.com>
2017-12-13 13:52:10 -0600

    Ahora con 99% menos carros spawneando en frente

---
+++ Assets/Scripts/DisableSpawners.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableSpawners : MonoBehaviour {
+    Transform parent;
+    GameObject carSpawners;
+	// Use this for initialization
+	void Start () {
+        parent = transform.parent;
+        carSpawners = parent.Find("CarSpawners").gameObject;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            carSpawners.SetActive(false);
+        }
+    }
+}
